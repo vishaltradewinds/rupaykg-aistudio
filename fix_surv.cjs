6c4f73175a9e785dfe775f9fd660b33c2b381222
Vishal Kumar <257167228+vishaltradewinds@users.noreply.github.com>
2026-08-29 14:18:48 +0530

    chore: remove obsolete one-time patch script

--- fix_surv.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('tests/two_process_survival_test.ts', 'utf8');
-content = content.replace(/eventType: "SURVIVAL_BLOCK"\n  \}, "0\.0\.123456", userId\); \/\/\n  \}\);/g, 'eventType: "SURVIVAL_BLOCK"\n  }, "0.0.123456", userId);');
-fs.writeFileSync('tests/two_process_survival_test.ts', content);
