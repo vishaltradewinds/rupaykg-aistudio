cffdea3615ce3e35bfad248b3179531c44672a93
Vishal Kumar <257167228+vishaltradewinds@users.noreply.github.com>
2026-08-29 14:18:28 +0530

    chore: remove obsolete one-time patch script

--- fix_pilot.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('tests/pilot_persistence_test.ts', 'utf8');
-content = content.replace(/  \}, "0\.0\.123456"\); \/\/\n  \}\);/g, '  }, "0.0.123456");');
-fs.writeFileSync('tests/pilot_persistence_test.ts', content);
