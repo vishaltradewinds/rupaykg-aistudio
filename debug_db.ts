94660a69cf2aa60689951c169a301b2e4ad7e34f
Vishal Kumar <257167228+vishaltradewinds@users.noreply.github.com>
2026-08-29 14:18:20 +0530

    chore: remove obsolete debug script

--- debug_db.ts
+++
@@ -1,8 +0,0 @@
-import { db } from "./src/db/index.ts";
-import { hedera_anchors } from "./src/db/schema.ts";
-
-async function run() {
-  const all = await db.select().from(hedera_anchors);
-  console.log("Anchors:", all);
-}
-run();
