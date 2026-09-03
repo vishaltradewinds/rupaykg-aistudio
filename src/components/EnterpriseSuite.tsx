d94d293851fce287bbb3681c3a6a492382f63cc3
Vishal Kumar <257167228+vishaltradewinds@users.noreply.github.com>
2026-09-03 17:42:13 +0530

    fix: route enterprise suite to canonical CCTS OS

--- src/components/EnterpriseSuite.tsx
+++ src/components/EnterpriseSuite.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
-import StakeholderDashboard from './StakeholderDashboard';
+import { CCTSCarbonOS } from './CCTSCarbonOS';
 
 interface EnterpriseSuiteProps {
   user: any;
   token?: string | null;
   onBackToDashboard: () => void;
   onNavigate?: (view: string) => void;
+  safeFetch?: (url: string, options?: RequestInit) => Promise<Response | null>;
+  safeParseJson?: (res: Response | null) => Promise<any>;
 }
 
-export default function EnterpriseSuite({ user, token = null, onNavigate }: EnterpriseSuiteProps) {
+export default function EnterpriseSuite({ user, token = null, safeFetch, safeParseJson }: EnterpriseSuiteProps) {
   const sessionToken = token || (typeof window !== 'undefined' ? localStorage.getItem('rupay_token') : null);
 
+  const fetcher = safeFetch || (async (url: string, options?: RequestInit) => fetch(url, options));
+  const parser = safeParseJson || (async (res: Response | null) => res ? res.json() : null);
+
   return (
-    <StakeholderDashboard
+    <CCTSCarbonOS
       user={user}
       token={sessionToken}
-      onNavigate={onNavigate}
+      safeFetch={fetcher}
+      safeParseJson={parser}
     />
   );
 }
